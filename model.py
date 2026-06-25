05e9573c06b645b3d5bd9e78d990c16232b2c946
ankush-nsut29 <ak2024iit@gmail.com>
2026-06-26 03:49:53 +0530

    adding qna and css change in form now its dark

--- model.py
+++ model.py
@@ -7,3 +7,17 @@ class student(db.Model):
     name=db.Column(db.String(50),nullable=False)
     email=db.Column(db.String(50),nullable=False,unique=True)
     password=db.Column(db.String(50),nullable=False)
+
+class question(db.Model):
+    id=db.Column(db.Integer,primary_key=True)
+    title=db.Column(db.String(100),nullable=False)
+    body=db.Column(db.Text)
+    user_id=db.Column(db.Integer,db.ForeignKey("student.id"))
+
+class answer(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.Text)
+    question_id = db.Column(db.Integer, db.ForeignKey("question.id"))
+    user_id = db.Column(db.Integer, db.ForeignKey("student.id"))
+
+db.relationship("answer",backref="question")
